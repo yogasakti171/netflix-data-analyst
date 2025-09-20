159062f8315d33a3e00e142b98b1c8f0143e2dd1
yogasakti171 <59883452+yogasakti171@users.noreply.github.com>
2025-09-20 14:47:29 +0700

    Mencari TV Series atau Film yang release terbanyak berdasarkan tahun@

--- netflix_dataset.py
+++ netflix_dataset.py
@@ -7,13 +7,18 @@ print(df.head())
 
 Q1 = df[df['Title']=='House of Cards'][['Show_Id','Director']]
 print(Q1)
+
+
 df['Release_Date'] = pd.to_datetime(df['Release_Date'], errors='coerce')
 df['Release_Years'] = df['Release_Date'].dt.year
 years_count = df['Release_Years'].value_counts().sort_index()
+years_label = years_count.index.astype(str)
 max_count = years_count.max()
 max_year = years_count.idxmax()
-print(max_count)
-print(max_year)
 
 plt.figure(figsize=(12,6))
+plt.bar(years_label,years_count.values, width=0.5, color='green')
+plt.xlabel("Tahun")
+plt.ylabel("Total")
+plt.show()
 
