97c35c3460eb2fe65859ca054ff32faeb2884026
yogasakti171 <59883452+yogasakti171@users.noreply.github.com>
2025-09-20 08:50:22 +0700

    Add project files

--- netflix_dataset.py
+++ netflix_dataset.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Netflix Dataset.csv")
+print(df.head())
+
+Q1 = df[df['Title']=='House of Cards'][['Show_Id','Director']]
+print(Q1)
+df['Release_Date'] = pd.to_datetime(df['Release_Date'], errors='coerce')
+df['Release_Years'] = df['Release_Date'].dt.year
+years_count = df['Release_Years'].value_counts().sort_index()
+max_count = years_count.max()
+max_year = years_count.idxmax()
+print(max_count)
+print(max_year)
+
+plt.figure(figsize=(12,6))
+
